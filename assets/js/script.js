5c0ec02f6f5b45e9c3347d4dc7f7719b27930270
sacarr98 <>
2023-10-11 09:16:35 +0000

    create array of questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// Quiz Questions
+const Questions = [{
+    q: "What is the national language of Canada?",
+    a: [{ text: "English", isCorrect: false },
+    { text: "French", isCorrect: false },
+    { text: "Dutch", isCorrect: true },
+    { text: "Spanish", isCorrect: false }
+    ]
+
+},
+{
+    q: "Brazil is the biggest producer of?",
+    a: [{ text: "Rice", isCorrect: false, isSelected: false },
+    { text: "Potatoes", isCorrect: false },
+    { text: "Oil", isCorrect: false },
+    { text: "Coffee", isCorrect: true }
+    ]
+
+},
+{
+    q: "How many colors in the Rainbow?",
+    a: [{ text: "Five", isCorrect: false },
+    { text: "Six", isCorrect: false },
+    { text: "Seven", isCorrect: true },
+    { text: "Eight", isCorrect: false }
+    ]
+
+},
+{
+    q: "Which religion has a God specified for each Rain, Money, Children, and Love?",
+    a: [{ text: "Islam", isCorrect: false },
+    { text: "Buddism", isCorrect: false },
+    { text: "Hinduism", isCorrect: true },
+    { text: "Jainism", isCorrect: false }
+    ]
+
+},
+{
+    q: "What is the currency of Pakistan?",
+    a: [{ text: "Pound", isCorrect: false },
+    { text: "Euro", isCorrect: false },
+    { text: "Riyal", isCorrect: false },
+    { text: "Rupee", isCorrect: true }
+    ]
+
+},
+{
+    q: "Which popular Disney show was Miley Cyrus part of?",
+    a: [{ text: "Hannah Montanna", isCorrect: true },
+    { text: "Two Broke Girls", isCorrect: false },
+    { text: "Live it like Beckham", isCorrect: false },
+    { text: "Suite life of Jack and Cody", isCorrect: false }
+    ]
+
+},
+
+
+];
